09949973713dc73c0930503ac939302cd580ab76
Hawa-codes <adediranhawa@gmail.com>
2025-12-12 11:35:43 +0100

    navbar complete

--- src/Component/Molecule/Navbar.jsx
+++ src/Component/Molecule/Navbar.jsx
@@ -4,6 +4,7 @@ import Logo from '../Atom/Logo'
 import bag_icon from '../Assets/bag-2.svg'
 import user_icon from '../Assets/user.svg'
 import './Molecule.css'
+import { NavLink } from 'react-router-dom'
 
 
 const Navbar = () => {
@@ -11,17 +12,38 @@ const Navbar = () => {
   return (
       <section className='navbar'>
        <Logo />
-       <div className="link">
-        <p>Product</p>
-        <p>Services</p>
-        <p>Article</p>
-        <p>AboutUs</p>
-          
-       </div>
-       <div className="icon">
+      <nav>
+        <NavLink 
+          to="/product" 
+          className="nav-link"
+        >
+          Product
+        </NavLink>
+
+        <NavLink 
+          to="/services" 
+          className="nav-link"
+        >
+          Services
+        </NavLink>
+
+        <NavLink 
+          to="/article" 
+          className="nav-link"
+        >
+          Article
+        </NavLink>
+        <NavLink 
+          to="/aboutUs" 
+          className="nav-link"
+        >
+          About Us
+        </NavLink>
+    </nav>
+      <div className="icon">
         <img src={bag_icon} alt="bag-icon" />
         <img src={user_icon} alt="user-icon" />
-       </div>
+      </div>
     </section>
   )
 }
